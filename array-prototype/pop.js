f76209f3c67020bff07be2f98cb76627999b7821
Kyle Rogers <kylerogers334@gmail.com>
2017-05-15 02:56:47 +0000

    Finish pop implimentation

---
+++ array-prototype/pop.js
@@ -0,0 +1,8 @@
+function pop(arr) {
+    const last = arr[arr.length - 1]
+    arr.length = arr.length - 1;
+    return last;
+}
+
+const x = [1,2,3,4,5]; // empty array inputs return undefined
+console.log(pop(x), x)
