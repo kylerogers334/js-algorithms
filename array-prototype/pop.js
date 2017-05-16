af9c830110b87a24a0e8fe6277318f86a5b44882
Kyle Rogers <kylerogers334@gmail.com>
2017-05-16 13:55:50 +0000

    semicolons...

--- array-prototype/pop.js
+++ array-prototype/pop.js
@@ -1,8 +1,8 @@
 function pop(arr) {
-    const last = arr[arr.length - 1]
+    const last = arr[arr.length - 1];
     arr.length = arr.length - 1;
     return last;
 }
 
 const x = [1,2,3,4,5]; // empty array inputs return undefined
-console.log(pop(x), x)
+console.log(pop(x), x);
