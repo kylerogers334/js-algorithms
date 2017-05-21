5b0e74ea2442bfa769ca981dc01b36fa230f5526
Kyle Rogers <kylerogers334@gmail.com>
2017-05-21 04:22:39 +0000

    Implement unshift

---
+++ array-prototype/unshift.js
@@ -0,0 +1,24 @@
+function unshift(arr) {
+    const unshiftedArray = [];
+    // first add the arguments to a new array
+    for (let i = 0; i < arguments.length; i++) {
+        unshiftedArray[i] = arguments[i+1];
+    }
+    // next add the original array
+    const newLength = unshiftedArray.length + arr.length - 1;
+    let j = 0;
+    for (let i = unshiftedArray.length - 1; i < newLength; i++) {
+        unshiftedArray[i] = arr[j];
+        j++;
+    }
+    // lastly modify the original array
+    for (let i = 0; i < newLength; i++) {
+        arr[i] = unshiftedArray[i];
+    }
+    return newLength;
+}
+
+const myArray = ["c", "d", "e"];
+
+console.log(unshift(myArray));
+console.log(myArray);
