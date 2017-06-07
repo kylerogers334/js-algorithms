ede69e890d01b5d296b6df823fe19f71f5ba146f
Kyle Rogers <kylerogers334@gmail.com>
2017-06-07 00:42:05 +0000

    Refactor and fix incorrect implementation

--- math-prototype/cos.js
+++ math-prototype/cos.js
@@ -1,20 +1,16 @@
-function cos(n) {
-    let x = n;
+function cos(x) {
     function factorial(f) {
         for (let i = f - 1; i > 0; i--) {
             f *= i;
         }
         return f;
     }
-    let first = true;
-    let sign = "-";
+    let n = 1 - Math.pow(x, 2) / 2;
+    let sign = "+";
+    
     /* 9 iteration taylor series is enough to converge in almost all situations */
-    for (let i = 2; i <= 18; i += 2) {
-        if (first) {
-            n = 1 - Math.pow(x, i) / factorial(i);
-            first = false;
-            continue;
-        } else if (sign === "-") {
+    for (let i = 4; i <= 18; i += 2) {
+    if (sign === "-") {
             n -= Math.pow(x, i) / factorial(i);
             sign = "+";
         } else {
