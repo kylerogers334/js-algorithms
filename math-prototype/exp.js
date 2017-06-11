82d70654e2d9e541921eca0169578a8b9870d2e8
Kyle Rogers <kylerogers334@gmail.com>
2017-06-11 07:00:51 +0000

    Correct the implementation to work with fractional exponents

--- math-prototype/exp.js
+++ math-prototype/exp.js
@@ -1,22 +1,25 @@
-function exp(n) {
-    let e = Math.E;
-    let x = e;
-    if (n === 0) {
-        return 1;
+function exp(x) {
+    function factorial(f) {
+        if (f === 0) return 1;
+        for (let i = f - 1; i > 0; i--) {
+            f *= i;
+        }
+        return f;
+    }
+
+    let sum = 0;
+    for (let i = 0; i < 50; i++) {
+        sum += (x ** i) / factorial(i);
     }
     
-    for (let i = 1; i < Math.abs(n); i++) {
-        x *= e;
-    }
-    
-    if (n < 0) {
-        return 1 / x;
-    }
-    
-    return x;
+    return sum;
 }
 
-console.log(exp(2));
+console.log(exp(-2/3));
+console.log(Math.exp(-2/3));
 console.log(exp(0));
+console.log(Math.exp(0));
 console.log(exp(-2));
-console.log(exp(11)); // Greater than 10 returns Infinity
+console.log(Math.exp(-2));
+console.log(exp(11));
+console.log(Math.exp(11));
