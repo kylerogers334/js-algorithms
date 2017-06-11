d3f9c700f4698d7446866709c0aa49f9e224b2b6
Kyle Rogers <kylerogers334@gmail.com>
2017-06-11 04:38:19 +0000

    Refactor

--- math-prototype/atanh.js
+++ math-prototype/atanh.js
@@ -5,20 +5,13 @@ function atanh(x) {
     if (x > 1 || x < -1) {
         return NaN;
     }
-    // cannot use a negative in the calculation,
-    // but output must be same sign as input
-    if (x < 0) {
-        var negative = -1;
-    }
-    
-    x = Math.abs(x);
     let n = 0;
     
     for (let i = 0; i < 10000; i++) {
-        n += (Math.pow(x, (2*i + 1)) / (2 * i + 1));
+        n += (Math.pow(Math.abs(x), (2 * i + 1)) / (2 * i + 1));
     }
-    
-    return (n * negative || n);
+
+    return x < 0 ? -n : n;
 }
 
 console.log(atanh(0.5));
