9cbec5b7ebd6006d98e0f3e5899bd3360fb2d00c
Kyle Rogers <kylerogers334@gmail.com>
2017-06-11 16:41:21 +0000

    Refactor

--- math-prototype/atanh.js
+++ math-prototype/atanh.js
@@ -5,13 +5,14 @@ function atanh(x) {
     if (x > 1 || x < -1) {
         return NaN;
     }
-    let n = 0;
+    
+    let sum = 0;
     
     for (let i = 0; i < 10000; i++) {
-        n += (Math.pow(Math.abs(x), (2 * i + 1)) / (2 * i + 1));
+        sum += (Math.pow(Math.abs(x), (2 * i + 1)) / (2 * i + 1));
     }
 
-    return x < 0 ? -n : n;
+    return x < 0 ? -sum : sum;
 }
 
 console.log(atanh(0.5));
