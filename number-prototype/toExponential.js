9d504a8760c8bbe6f5a42bcacde6db91c450fe9b
Kyle Rogers <kylerogers334@gmail.com>
2017-06-17 23:28:35 +0000

    works without decimal place parameter

--- number-prototype/toExponential.js
+++ number-prototype/toExponential.js
@@ -1,25 +1,22 @@
 function toExponential(n, x=0) {
-    let decimalPlaces = String(n).split('.')[0].length - 1;
-    let str = String(n)
-        .replace(/\./g, '')
-        .split('');
-        
-    // Cannot chain on splice because
-    // spice returns removed items.
-    str.splice(1, 0 , '.'); 
+    if (n < 0 || isNaN(n) || n === Infinity) {
+        return n;
+    }
+    
+    const decimalPlaces = String(n).split('.')[0].length - 1;
+    let str = String(n).split(".").join("");
+    str = str[0] + "." + str.slice(1);
     
     // e+decimalPlaces is auto appended for
     // large numbers.
-    if (str.length > 17) {
-        return str.join('');
+    if (str.length > 19) {
+        return str;
     }
     
-    return str 
-        .join('')
-        .concat(`e+${decimalPlaces}`);
+    return str + `e+${decimalPlaces}`;
 }
-const numObj = 801231239;
-const numObj = 80123123912312312312123;
-
-console.log(toExponential(801231239));
-console.log(toExponential(80123123912312312312123));
+console.log(toExponential(8012312399081230123123123));
+console.log(toExponential(123456789012345678901));
+console.log(123456789012345678901 .toExponential());
+console.log(812.38 .toExponential());
+console.log(toExponential(812.38));
