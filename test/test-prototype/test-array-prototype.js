ad2e9de4c063939955d704e16ce46a141dbb9fdd
Kyle Rogers <kylerogers334@gmail.com>
2017-06-23 02:52:26 +0000

    Add skips to every test(for now)

--- test/test-prototype/test-array-prototype.js
+++ test/test-prototype/test-array-prototype.js
@@ -1,122 +1,123 @@
+'use strict';
 const expect = require('chai').expect;
 const glob = require( 'glob' )
     , path = require( 'path' );
-    
+
 glob.sync( '../../array-prototype/concat.js' ).forEach( function( file ) {
     const p = path.basename(file).split('.')[0];
     global[p] = require(path.resolve(file));
 });
 
 describe('array-prototype algorithms', function() {
-    it('correctly implements concat', function() {
+    it.skip('correctly implements concat', function() {
         
     });
     
-    it('correctly implements copyWithin', function() {
+    it.skip('correctly implements copyWithin', function() {
         
     });
     
-    it('correctly implements entries', function() {
+    it.skip('correctly implements entries', function() {
         
     });
     
-    it('correctly implements every', function() {
+    it.skip('correctly implements every', function() {
         
     });
     
-    it('correctly implements fill', function() {
+    it.skip('correctly implements fill', function() {
         
     });
 
-    it('correctly implements filter', function() {
+    it.skip('correctly implements filter', function() {
         
     });
     
-    it('correctly implements find', function() {
+    it.skip('correctly implements find', function() {
         
     });
     
-    it('correctly implements findIndex', function() {
+    it.skip('correctly implements findIndex', function() {
         
     });
     
-    it('correctly implements forEach', function() {
+    it.skip('correctly implements forEach', function() {
         
     });
     
-    it('correctly implements includes', function() {
+    it.skip('correctly implements includes', function() {
         
     });
     
-    it('correctly implements indexOf', function() {
+    it.skip('correctly implements indexOf', function() {
         
     });
 
-    it('correctly implements isArray', function() {
+    it.skip('correctly implements isArray', function() {
         
     });
     
-    it('correctly implements join', function() {
+    it.skip('correctly implements join', function() {
         
     });
     
-    it('correctly implements keys', function() {
+    it.skip('correctly implements keys', function() {
         
     });
     
-    it('correctly implements lastIndexOf', function() {
+    it.skip('correctly implements lastIndexOf', function() {
         
     });
     
-    it('correctly implements map', function() {
+    it.skip('correctly implements map', function() {
         
     });
     
-    it('correctly implements of', function() {
+    it.skip('correctly implements of', function() {
         
     });
     
-    it('correctly implements pop', function() {
+    it.skip('correctly implements pop', function() {
         
     });
     
-    it('correctly implements push', function() {
+    it.skip('correctly implements push', function() {
         
     });
     
-    it('correctly implements reduce', function() {
+    it.skip('correctly implements reduce', function() {
         
     });
     
-    it('correctly implements reduceRight', function() {
+    it.skip('correctly implements reduceRight', function() {
         
     });
     
-    it('correctly implements reverse', function() {
+    it.skip('correctly implements reverse', function() {
         
     });
     
-    it('correctly implements shift', function() {
+    it.skip('correctly implements shift', function() {
         
     });
     
-    it('correctly implements some', function() {
+    it.skip('correctly implements some', function() {
         
     });
     
-    it('correctly implements sort', function() {
+    it.skip('correctly implements sort', function() {
         
     });
     
-    it('correctly implements splice', function() {
+    it.skip('correctly implements splice', function() {
         
     });
     
-    it('correctly implements toString', function() {
+    it.skip('correctly implements toString', function() {
         
     });
     
-    it('correctly implements unshift', function() {
+    it.skip('correctly implements unshift', function() {
         
     });
 });
