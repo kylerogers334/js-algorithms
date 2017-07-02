8db646517a7618ad30b530eee0fa2fbcdf1a13e9
Kyle Rogers <kylerogers334@gmail.com>
2017-07-02 17:47:05 +0000

    Implement getMonth

---
+++ date-prototype/getMonth.js
@@ -0,0 +1,26 @@
+function getMonth(d) {
+    // Turn date into number of days in UNIX time.
+    d = Math.floor(Number(d) / 2592000000);
+    
+    while (d >12) {
+        d %= 12;
+    }
+    
+    // Using this method, the return value must be offset
+    // as UNIX time begins on a Thursday.
+    switch (d) {
+        case 8: return 0; // January
+        case 9: return 1;
+        case 10: return 2;
+        case 11: return 3;
+        case 0: return 4;
+        case 1: return 5;
+        case 2: return 6;
+        case 3: return 7;
+        case 4: return 8;
+        case 5: return 9;
+        case 6: return 10;
+        case 7: return 11; // December
+    }
+    return d;
+}
