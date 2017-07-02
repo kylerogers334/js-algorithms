ad1044130d1692804b09b64763839b48f0ea523b
Kyle Rogers <kylerogers334@gmail.com>
2017-07-02 20:58:38 +0000

    file rename

--- date-prototype/getTimeZoneOffSet.js
+++
@@ -1,6 +0,0 @@
-function getTimeZoneOffSet(d) {
-    const UTCOffSet = String(d).split(' ')[5].replace(/[a-zA-Z]/g, '');
-    return  60 * (Number(UTCOffSet) / 100);
-}
-var x = new Date;
-console.log(getTimeZoneOffSet(x))
