a19596ee88e43edf5337d128f5f11496742b1934
Kyle Rogers <kylerogers334@gmail.com>
2017-07-03 03:53:14 +0000

    Implement setHours

---
+++ date-prototype/setHours.js
@@ -0,0 +1,21 @@
+const monthToInt = require('./monthToInt');
+
+function setHours(d, hourVal, minVal, secVal, msVal) {
+
+    // Milliseconds must be extracted before coercion.
+    // MS is lost in coercion.
+    const ms = msVal || String(Number(d)).slice(10, 13);
+
+    d = String(d).split(' ');
+
+    const dayTime = d[4].split(':');
+
+    const year  = d[3];
+    const month = monthToInt(d[1]);
+    const day   = d[2];
+    const hour  = hourVal || dayTime[0];
+    const min   = minVal || dayTime[1];
+    const sec   = secVal || dayTime[2];
+
+    return Number(new Date(year, month, day, hour, min, sec, ms));
+}
