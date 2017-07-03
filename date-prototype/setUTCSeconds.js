9f4e79ad1419e3eeec6c4091e86605707009471b
Kyle Rogers <kylerogers334@gmail.com>
2017-07-03 18:16:03 +0000

    Implement setUTCSeconds

---
+++ date-prototype/setUTCSeconds.js
@@ -0,0 +1,20 @@
+const monthToInt = require('./monthToInt');
+
+function setUTCSeconds(d, sec) {
+
+    // Milliseconds must be extracted before coercion.
+    // MS is lost in coercion.
+    const ms = String(Number(d)).slice(10, 13);
+
+    d = String(d).split(' ');
+
+    const dayTime = d[4].split(':');
+
+    const year  = d[3];
+    const month = monthToInt(d[1]);
+    const day   = d[2];
+    const hour  = dayTime[0];
+    const min   = dayTime[1];
+
+    return Number(new Date(year, month, day, hour, min, sec, ms));
+}
