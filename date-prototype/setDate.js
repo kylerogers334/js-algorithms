8756da98dd37dd9429b3c9a589197a71b3249ccc
Kyle Rogers <kylerogers334@gmail.com>
2017-07-03 00:21:29 +0000

    Implement setDate

---
+++ date-prototype/setDate.js
@@ -0,0 +1,20 @@
+const monthToInt = require('./monthToInt');
+
+function setDate(d, day) {
+
+    // Milliseconds must be extracted before coercion.
+    // MS is lost in coercion.
+    const ms = String(Number(d)).slice(10, 13);
+
+    d = String(d).split(' ');
+
+    const dayTime = d[4].split(':');
+
+    const year  = d[3];
+    const month = monthToInt(d[1]);
+    const hour  = dayTime[0];
+    const min   = dayTime[1];
+    const sec   = dayTime[2];
+
+    return Number(new Date(year, month, day, hour, min, sec, ms));
+}
