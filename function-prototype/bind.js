d91b56536001eccba85276260be249a7e5b4d414
Kyle Rogers <kylerogers334@gmail.com>
2017-07-04 22:50:58 +0000

    add module exports

---
+++ function-prototype/bind.js
@@ -0,0 +1,8 @@
+function bind(fn, thisArg) {
+    const args = Array.from(arguments).slice(1);
+    const boundFn = function() { return fn.apply(thisArg, args) };
+    boundFn.prototype = new function() {};
+    return boundFn;
+}
+
+module.exports = bind;
