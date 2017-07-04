645b92e4501ecf3df11753327c786dd8dffb1230
Kyle Rogers <kylerogers334@gmail.com>
2017-07-04 21:08:35 +0000

    Implement repeat

---
+++ string-prototype/repeat.js
@@ -0,0 +1,15 @@
+function repeat(str, count) {
+    // Cannot exceed the max string length.
+    if (count < 0 || count > 1 << 28 ) {
+        throw new RangeError('Invalid length');
+    }
+    
+    count = Math.floor(count);
+    let newStr = "";
+    
+    for (let i = 0; i++ < count;) {
+        newStr += str;
+    }
+    
+    return newStr;
+}
