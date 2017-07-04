82444cf83f352271106d14c37ea73fa6d2d64a02
Kyle Rogers <kylerogers334@gmail.com>
2017-07-04 22:35:52 +0000

    add module exports

--- function-prototype/apply.js
+++ function-prototype/apply.js
@@ -1,5 +1,6 @@
 function apply(fn, thisArg, argsArray) {
     const args = arguments[2];
-    console.log('args', args);
     return (fn.bind(thisArg))(...args);
 }
+
+module.exports = apply;
--- function-prototype/call.js
+++ function-prototype/call.js
@@ -2,3 +2,5 @@ function call(fn, thisArg) {
     const args = Array.from(arguments).slice(2);
     return (fn.bind(thisArg))(...args);
 }
+
+module.exports = call;
