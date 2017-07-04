591e2ac9ce0c2927d9b8d87d6955561afc9bcfae
Kyle Rogers <kylerogers334@gmail.com>
2017-07-04 23:11:17 +0000

    Implement substring

---
+++ string-prototype/substring.js
@@ -0,0 +1,18 @@
+function substring(str, start=0, end=str.length) {
+    if (start === end) return "";
+    if (start < 0 || isNaN(Number(start))) start = 0;
+    if (end < 0 || isNaN(Number(end))) end = 0;
+    if (end > str.length) end = str.length;
+
+    // parameter swap when weird paraters are given
+    if (start > end) {
+        [start, end] = [end, start];
+    }
+
+    let newStr = "";
+    for (var i = start; i < end; i++) {
+        newStr += str[i];
+    }
+
+    return newStr;
+}
