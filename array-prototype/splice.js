e56182e2d674b54f0f854625dc314c4629841a22
Kyle Rogers <kylerogers334@gmail.com>
2017-07-04 00:16:42 +0000

    Implement splice

--- array-prototype/splice.js
+++ array-prototype/splice.js
@@ -1,17 +1,17 @@
 function splice(arr, start, stop) {
     const newArr = [];
-    
+
     let j = 0;
     for (let i = start; i < stop; i++) {
         newArr[j] = arr[i];
-        arr[i] = "remove";
+        delete arr[i];
         j++;
     }
-    
+
     const updatedArr = [];
     j = 0;
     for(let i = 0; i < arr.length; i++) {
-        if (arr[i] !== "remove") {
+        if (arr[i] !== undefined) {
             updatedArr[j] = arr[i];
             j++;
         }
