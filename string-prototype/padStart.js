ffa726bfaed22f40c93c54c3a5f5b5bbaef31256
Kyle Rogers <kylerogers334@gmail.com>
2017-07-05 02:09:04 +0000

    Implement padStart

---
+++ string-prototype/padStart.js
@@ -0,0 +1,9 @@
+function padStart(str, targetLength, padStr=" ") {
+    if (targetLength < str.length) return str;
+    if (padStr.length > targetLength) return padStr.slice(0, targetLength);
+
+    const maxPad = targetLength - str.length;
+    padStr = padStr.repeat(targetLength/padStr.length).slice(0, maxPad);
+
+    return padStr + str;
+}
