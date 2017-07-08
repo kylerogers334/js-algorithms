762616300f400d39ea6b7deab6891acf7510bc68
Kyle Rogers <kylerogers334@gmail.com>
2017-07-08 21:09:35 +0000

    Add correct implementation

--- string-prototype/includes.js
+++ string-prototype/includes.js
@@ -1,13 +1,12 @@
 function includes(str, item, start = 0) {
+    const l = item.length;
+    
     for (let i = start; i < str.length; i++) {
-        if (str[i] === item) return true;
+        if ((i + l) > str.length) break;
+        if (str.slice(i, i + l) === item) return true;
     }
 
     return false;
 }
 
-const myStr = "here's johnny";
-
-console.log(includes(myStr, "j")); // true
-console.log(includes(myStr, "x")); // false
-console.log(includes(myStr, "j", 10)); //false
+module.exports = includes;
