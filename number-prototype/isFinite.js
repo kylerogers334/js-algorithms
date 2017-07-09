3e006005b5624198b04598aaf9c28fac6e8e9be7
Kyle Rogers <kylerogers334@gmail.com>
2017-07-09 19:57:23 +0000

    add module exports

--- number-prototype/isFinite.js
+++ number-prototype/isFinite.js
@@ -2,4 +2,4 @@ function isFinite(n) {
     return (typeof n === "number" && n !== Infinity && n !== -Infinity && isNaN(n) === false);
 }
 
-console.log(isFinite(NaN));
+module.exports = isFinite;
--- number-prototype/isInteger.js
+++ number-prototype/isInteger.js
@@ -2,11 +2,4 @@ function isInteger(val) {
     return (Math.floor(val) === val && val !== Infinity && val !== -Infinity);
 }
 
-console.log(isInteger(1212323)); // true
-console.log(isInteger(-980009123)); // true
-console.log(isInteger(-5123123.1231)); // false
-console.log(isInteger(NaN)); // false
-console.log(isInteger(Infinity)); // false
-console.log(isInteger(-Infinity)); // false
-console.log(isInteger(true)); // false
-console.log(isInteger(false)); // false
+module.exports = isInteger;
