0d323331cbb483f15ddbf673a223a269ab26a3f8
Kyle Rogers <kylerogers334@gmail.com>
2017-07-09 21:00:54 +0000

    Change output val to number instead of string

--- date-prototype/getMilliseconds.js
+++ date-prototype/getMilliseconds.js
@@ -1,7 +1,7 @@
 function getMilliseconds(d) {
     // Milliseconds not available on the string form of date instance,
     // requiring this method.
-    return String(Number(d)).slice(10, 13);
+    return Number(String(Number(d)).slice(10, 13));
 }
 
 module.exports = getMilliseconds;
