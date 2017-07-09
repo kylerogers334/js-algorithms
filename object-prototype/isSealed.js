ae0292d0f01ba53305fb615171acec6bb41341d1
Kyle Rogers <kylerogers334@gmail.com>
2017-07-09 04:48:01 +0000

    Fix Implementation

--- object-prototype/isSealed.js
+++ object-prototype/isSealed.js
@@ -1,18 +1,10 @@
 function isSealed(obj) {
     'use strict';
     try {
-        // throwing an error here means frozen
-        // Object.isSealed(frozenObj) => true
-        obj[Object.keys(obj)[0]] = 'something else';
-        try {
-            // checking if sealed and not frozen
-            delete obj[Object.keys(obj)];
-            return false;
-        } catch (e) { 
-            return true; 
-        }
-    } catch (e) {
-        return true;
+        obj.reallyrandomassnamethatshouldntbetaken908908123 = 'bar';
+        return false;
+    } catch (e) { 
+        return true; 
     }
 }
 
