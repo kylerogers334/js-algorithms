4d9e3de3f505ad7a447d497fd8f947b173a2f41a
Kyle Rogers <kylerogers334@gmail.com>
2017-07-10 22:29:23 +0000

    Implement round

---
+++ math-prototype/round.js
@@ -0,0 +1,17 @@
+function round(n) {
+    const isPos = n >= 0;
+
+    n = String(n).split('.');
+    const wholeNum = Number(n[0]);
+    const decimals = "0." + n[1];
+
+    if (decimals >= 0.5) {
+        if (isPos) {
+            return wholeNum + 1;
+        }
+        return wholeNum - 1;
+    }
+
+    return wholeNum;
+}
+
