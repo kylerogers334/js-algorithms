f691c1062ed2a105daa32a61a1f598f6bd11f13b
Kyle Rogers <kylerogers334@gmail.com>
2017-07-13 14:51:09 +0000

    Implement toString

---
+++ number-prototype/toString.js
@@ -0,0 +1,16 @@
+function toString(n, r=10) {
+        if (!n) return '0';
+        if (n < 0) return '-' + toString(-n);
+        let string = '';
+        while (n) {
+                const digit = n % r;
+                n = (n - digit) / r;
+                const char = String.fromCodePoint(
+                    digit < 10 ? 0x30 + digit
+                    : 0x41 + digit - 10
+                );
+                string = char + string;
+        }
+        return string;
+}
+
