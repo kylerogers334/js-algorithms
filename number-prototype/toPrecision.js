146e24b3795781f735456280bf1d662ae7771091
Kyle Rogers <kylerogers334@gmail.com>
2017-07-15 16:27:17 +0000

    Implement toPrecision

--- number-prototype/toPrecision.js
+++ number-prototype/toPrecision.js
@@ -3,20 +3,33 @@ function toPrecision(n, sigfig) {
         return String(n);
     }
     
-    if (sigfig > 100 || sigfig < 0) {
+    sigfig = Math.round(sigfig);
+    
+    if (sigfig > 100 || sigfig < 1) {
         throw new RangeError('toPrecision() argument must be between 1 and 100');
     }
-    
-    let arr = String(n).split('.');
-    let str;
-    
-    if (arr.length === 1) { // no decimal places
-        str = arr[0];
-        let e = str.length - 1;
-        return str[0] + "." + str.slice(1, sigfig) + 'e+' + e;
+
+    if (!n) return '0';
+    let exponent = 0;
+    while (n > 10) {
+        ++exponent;
+        n /= 10;
     }
-    
-   
+
+    while (n < 1) {
+        --exponent;
+        n *= 10;
+    }
+
+    const rounded = String(Math.round(n * (Math.pow(10, sigfig - 1))));
+    const toDecimal = rounded.substring(0, 1) + 
+                        (sigfig === 1 ? '': '.') +
+                        rounded.substring(1);
+
+    if (!exponent) return toDecimal;
+
+    // Coercion to pass tests
+    return String(Number(toDecimal + 'e' + exponent));
 }
 
-console.log(toPrecision(12345, 3))
+module.exports = toPrecision;
