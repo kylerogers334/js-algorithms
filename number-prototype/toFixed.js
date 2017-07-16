028ce2d76fb8370e4380332549579fd3a81fd721
Kyle Rogers <kylerogers334@gmail.com>
2017-07-16 22:09:15 +0000

    Implement toFixed

---
+++ number-prototype/toFixed.js
@@ -0,0 +1,24 @@
+function toFixed(n, digits) {
+    if (!digits) {
+        return Math.round(n);
+    }
+
+    const split = String(n).split('.');
+    const int = split[0];
+    let decimals = split[1] || '';
+    const dLen = decimals.length;
+
+    for (var i = decimals.length; i < digits; i++) {
+        decimals += '0';
+    }
+
+    if (dLen < digits) {
+        return int + '.' + decimals;
+    }
+
+    const fixed = decimals.slice(0, digits + 1) / 10;
+
+    return int + '.' + Math.round(fixed);
+}
+
+module.exports = toFixed;
