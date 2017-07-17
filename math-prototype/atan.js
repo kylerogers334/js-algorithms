92b0c110ae648048dc965d23aad42e8cf2d4c336
Kyle Rogers <kylerogers334@gmail.com>
2017-07-17 03:40:02 +0000

    Implement arc tan

--- math-prototype/atan.js
+++ math-prototype/atan.js
@@ -1,16 +1,11 @@
 function atan(x) {
-    // cannot use a negative in the calculation,
-    // but output must be same sign as input
-    let negative;
-    if (x < 0) {
-        negative = -1;
+    let sum = 0;
+    for (let i = 0; i <80; i++) {
+        sum += (Math.pow(-1, i) * Math.pow(x, (2 * i + 1))) / (2 * i + 1);
     }
-    
-    x = Math.abs(x);
-    let n = 0;
-
-    for (let i = 0; i < 99; i++) {
-        n += (Math.pow(-1, i) * Math.pow(x, (2 * i + 1))) / (2 * i + 1);
-    }
-    return (n * negative || n);
+    return x < 0 ? -sum : sum;
 }
+
+
+console.log(atan(0.11))
+console.log(Math.atan(0.11))
