36495e7da0d23c04552b756acff383423397faf7
Kyle Rogers <kylerogers334@gmail.com>
2017-07-17 19:33:44 +0000

    Implement log base 2

---
+++ math-prototype/log2.js
@@ -0,0 +1,12 @@
+function log(x) {
+    let sum = 0;
+
+    for (let i = 0; i < 1000; i++) {
+        sum += (1 / (2 * i + 1)) * 
+                Math.pow(((x - 1) / (x + 1)), (2 * i + 1));
+    }
+
+    return 2 * sum / Math.LN2;
+}
+
+module.exports = log;
