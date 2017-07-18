9a5ab252b218a13e39bebb67f81e8c270386dfa0
Kyle Rogers <kylerogers334@gmail.com>
2017-07-18 15:55:19 +0000

    add missed semicolon

--- function-prototype/bind.js
+++ function-prototype/bind.js
@@ -1,6 +1,6 @@
 function bind(fn, thisArg) {
     const args = Array.from(arguments).slice(1);
-    const boundFn = function() { return fn.apply(thisArg, args) };
+    const boundFn = function() { return fn.apply(thisArg, args); };
     boundFn.prototype = new function() {};
     return boundFn;
 }
