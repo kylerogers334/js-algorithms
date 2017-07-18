55daffbfaca1c70518feaa155314efb286904a5e
Kyle Rogers <kylerogers334@gmail.com>
2017-07-18 01:32:47 +0000

    Handle negative exponents

--- math-prototype/pow.js
+++ math-prototype/pow.js
@@ -1,11 +1,12 @@
 function pow(base, exponent){
+    const e = Math.abs(exponent)
     let n = base;
     
-    for (let i = 1; i < exponent; i++) {
+    for (let i = 1; i < e; i++) {
         n *= base;
     }
     
-    return n;
+    return exponent < 0 ? 1/n : n;
 }
 
 module.exports = pow;
