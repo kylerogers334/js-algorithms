8cbf7cd23b193939902970615e0e475c8c59e30f
Kyle Rogers <kylerogers334@gmail.com>
2017-07-18 01:32:04 +0000

    handle negative numbers

--- math-prototype/atan.js
+++ math-prototype/atan.js
@@ -1,7 +1,8 @@
 function atan(x) {
+    const n = Math.abs(x);
     let sum = 0;
     for (let i = 0; i <80; i++) {
-        sum += (Math.pow(-1, i) * Math.pow(x, (2 * i + 1))) / (2 * i + 1);
+        sum += (Math.pow(-1, i) * Math.pow(n, (2 * i + 1))) / (2 * i + 1);
     }
     return x < 0 ? -sum : sum;
 }
