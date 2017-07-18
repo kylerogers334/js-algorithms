a91349af48dbb5bd9394a7f85a4598a3bce80f98
Kyle Rogers <kylerogers334@gmail.com>
2017-07-18 16:31:37 +0000

    Fix the implementation with regards to timezones not affecting ms

--- date-prototype/getUTCMilliseconds.js
+++ date-prototype/getUTCMilliseconds.js
@@ -1,9 +1,7 @@
 const toUTCString = require('./toUTCString');
 
 function getUTCMilliseconds(d) {
-    // Milliseconds not available on the string form of date instance,
-    // requiring this mess.
-    return Number(String(Number(toUTCString(d))).slice(10, 13));
+    return +String(Number(d)).slice(10);
 }
 
 module.exports = getUTCMilliseconds;
