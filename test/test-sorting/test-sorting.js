92da7d29a6ef4353a9601f38e886a09f3389ea0b
Kyle Rogers <kylerogers334@gmail.com>
2017-07-18 19:27:08 +0000

    add tests for bubble and insertion sort

--- test/test-sorting/test-sorting.js
+++ test/test-sorting/test-sorting.js
@@ -9,5 +9,16 @@ glob.sync( './sorting/*.js' ).forEach( function( file ) {
 });
 
 describe('sorting algorithms', function() {
+    const sortedArr = [0,1,2,3,4,5,6,7,8,9];
+    const revArr = [9,8,7,6,5,4,3,2,1,0];
+    const arr = [9,4,3,0,1,8,2,7,6,5];
     
+    it('sorts using bubble sort', function() {
+        expect(bubbleSort.bubbleSort(arr)).to.deep.equal(sortedArr);
+        expect(bubbleSort.bubbleSortReverse(arr)).to.deep.equal(revArr);
+    });
+    
+    it('sorts using insertion sort', function() {
+        expect(insertionSort(arr)).to.deep.equal(sortedArr);
+    });
 });
