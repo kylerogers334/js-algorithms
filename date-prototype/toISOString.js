2747655707f07f8182bf8370fda786afdab8add9
Kyle Rogers <kylerogers334@gmail.com>
2017-07-18 18:11:20 +0000

    Re-implement correct toISOString

--- date-prototype/toISOString.js
+++ date-prototype/toISOString.js
@@ -1,15 +1,17 @@
 const toJSON = require('./toJSON');
 const toUTCString = require('./toUTCString');
+const monthtoInt = require('./monthToInt');
 
 function toISOString(d) {
     // Converting to UTC before JSON will
     // remove the milliseconds, have to work around.
-    const ms = String(Number(d)).slice(10, 13);
-    
-    d = toUTCString(d);
-    d = toJSON(d);
-
-    return d.slice(0, 20) + ms + 'Z';
+    const ms = String(Number(d)).slice(10, 13).padStart(3, '0');
+    const arr = String(d).split(' ');
+    const formattedMonth = String(monthtoInt(arr[1]) + 1).padStart(2, '0');
+    return  arr[3] + '-' +
+            formattedMonth + '-' +
+            arr[2] + 'T' + arr[4] + 
+            '.' + ms + 'Z';
 }
 
 module.exports = toISOString;
