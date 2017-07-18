2a4aebf1feb1344d0fb76d7e9ef74dbd00cd71e7
Kyle Rogers <kylerogers334@gmail.com>
2017-07-18 15:51:51 +0000

    change to single quotes

--- array-prototype/join.js
+++ array-prototype/join.js
@@ -1,7 +1,7 @@
-function join(arr, separator=",") {
-    let str = "";
+function join(arr, separator=',') {
+    let str = '';
     for (let i = 0; i < arr.length; i++) {
-        if (i === arr.length - 1) separator = "";
+        if (i === arr.length - 1) separator = '';
         str += (arr[i] + separator);
     }
     return str;
